7893a9440b9b39f764e49abc19dd639380ef5cc0
lamvng <lam.nguyenvan1997@gmail.com>
2020-03-28 18:13:35 +0700

    Preprocess and normalize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import settings
-from preprocess import load_file
+from preprocess import load_file, normalize
 settings.init()
 
 df = load_file.load_dataframe()
+
+one_hot_features = ['protocol_type', 'service', 'flag']
+df = normalize.one_hot_encode(df, one_hot_features)
--- preprocess/normalize.py
+++ preprocess/normalize.py
@@ -4,34 +4,33 @@ from sklearn import preprocessing
 
 # One-hot encoding for a column of a dataframe
 # Column: string
-def one_hot_encode(df, one_hot_columns):
-    loc_last_column = len(df.columns) - 1  # Index of last column of df
-    for col_name in one_hot_columns:
-    # drop_first: Make one of the feature goes [0,0,0,...0]
+def one_hot_encode(df, one_hot_features):
+    loc_last_column = len(df.columns)
+    loc_insert = df.columns.get_loc('protocol_type')  # Original index of the first categorical columns in df
+    for col_name in one_hot_features:
+        # drop_first: Make one of the feature goes [0,0,0,...0]
         feature = pd.get_dummies(df[col_name], prefix=col_name, drop_first=True)
         df = pd.concat([df, feature], axis=1)
 
-        # Move the one-hotted-column to its original position after concatenating
-        cols_df = df.columns.tolist()
-        cols_feature = feature.columns.tolist()
-        loc_column = df.columns.get_loc(col_name)  # Original ndex of current column of df
-        for added_col in cols_feature: # Iterate by column name
-            cols_df.insert(loc_column, cols_df.pop(cols_df.index(added_col))) # Pop the current col, then add to the original position
-        df = df.reindex(columns=cols_df)
+    # Move the added one-hot columns to their original index
+    moved_df = df[df.columns[loc_last_column:]] # Added one-hot columns
+    cols_df = df.columns.tolist()
+    cols_features = moved_df.columns.tolist()
+    for (count, cols_feature) in enumerate(cols_features):
+        # Pop the current col, then add to the original position
+        cols_df.insert(loc_insert + count, cols_df.pop(cols_df.index(cols_feature)))
+    df = df.reindex(columns=cols_df)
 
     # Delete categorical columns after being one-hot-encoded
-    for col_name in one_hot_columns:
+    for col_name in one_hot_features:
         df.drop([col_name], axis=1, inplace=True)
-
     return df
-
-def split_train_test(df):
 
 def normalize(df):
     # Training and Test
     # One hot encoding for protocol_type, service, flag
-    one_hot_columns = ['protocol_type', 'service', 'flag']
-    df = one_hot_encode(df, one_hot_columns)
+    one_hot_features = ['protocol_type', 'service', 'flag']
+    df = one_hot_encode(df, one_hot_features)
     scaler = preprocessing.MinMaxScaler()
     Y = df.iloc[:, -2] # Label # Series
     X = df.drop(['label', 'score'], axis='columns') # Training # Dataframe
